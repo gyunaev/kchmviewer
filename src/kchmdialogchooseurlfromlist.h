695b144d34a905aa82eae6eefd4e619a90b9f71b
gyunaev <gyunaev@users.noreply.github.com>
2005-04-23 23:06:26 +0000

    *** empty log message ***

---
+++ src/kchmdialogchooseurlfromlist.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by tim   *
+ *   tim@krasnogorsk.ru   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef KCHMDIALOGCHOOSEURLFROMLIST_H
+#define KCHMDIALOGCHOOSEURLFROMLIST_H
+
+#include <qdialog.h>
+#include <qlistview.h>
+
+/**
+@author tim
+*/
+class KCHMDialogChooseUrlFromList : public QDialog
+{
+Q_OBJECT
+public:
+    KCHMDialogChooseUrlFromList (const QStringList& urls, const QStringList& titles, QWidget* parent);
+	QString getSelectedItemUrl()	{ return m_acceptedurl; }
+
+private slots:
+	void onDoubleClicked( QListViewItem * item, const QPoint &, int );
+	void onCurrentChanged ( QListViewItem * item);
+
+private:
+	QListView *	m_urlsList;
+	QString		m_acceptedurl;
+};
+
+#endif

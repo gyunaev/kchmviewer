c57148a8f34e07d87114214f40a939ce23a791ff
gyunaev <gyunaev@users.noreply.github.com>
2005-05-04 14:40:17 +0000

    - adding khtmlpart view window

---
+++ src/kchmviewwindow_khtmlpart.cpp
@@ -0,0 +1,98 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Georgy Yunaev                                   *
+ *   tim@krasnogorsk.ru                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <qregexp.h>
+#include <qstring.h>
+#include <qdir.h>
+
+#include "kde-qt.h"
+#include "kchmmainwindow.h"
+#include "kchmviewwindow_khtmlpart.h"
+
+#if defined (USE_KDE)
+
+KCHMViewWindow_KHTMLPart::KCHMViewWindow_KHTMLPart( QWidget * parent )
+	: KHTMLPart ( parent ), KCHMViewWindow ( parent )
+{
+	m_zoomfactor = 0;
+	invalidate();
+	
+//	connect( this, SIGNAL( linkClicked (const QString &) ), this, SLOT( slotLinkClicked(const QString &) ) );
+}
+
+
+KCHMViewWindow_KHTMLPart::~KCHMViewWindow_KHTMLPart()
+{
+}
+
+bool KCHMViewWindow_KHTMLPart::openPage (const QString& url)
+{
+	QString fullurl = "ms-its:" + ::mainWindow->getOpenedFileName() + "::" + url;
+	openURL ( KURL(fullurl) );
+	return true;
+}
+
+void KCHMViewWindow_KHTMLPart::setZoomFactor( int zoom )
+{
+	m_zoomfactor = zoom;
+/*	
+	if ( zoom < 0 )
+		QTextBrowser::zoomOut( -zoom );
+	else if ( zoom > 0 )
+		QTextBrowser::zoomIn( zoom);
+*/
+}
+
+void KCHMViewWindow_KHTMLPart::invalidate( )
+{
+	m_zoomfactor = 0;
+//	KCHMViewWindow::invalidate( );
+}
+
+int KCHMViewWindow_KHTMLPart::getScrollbarPosition( )
+{
+	return 0; /* contentsY (); */
+}
+
+void KCHMViewWindow_KHTMLPart::setScrollbarPosition( int pos )
+{
+//	setContentsPos (0, pos);
+}
+
+void KCHMViewWindow_KHTMLPart::addZoomFactor( int value )
+{
+	setZoomFactor( m_zoomfactor + value);
+}
+
+void KCHMViewWindow_KHTMLPart::slotLinkClicked( const QString & newlink )
+{
+	bool notused;
+	emit signalLinkClicked (newlink, notused);
+}
+
+void KCHMViewWindow_KHTMLPart::emitSignalHistoryAvailabilityChanged( bool enable_backward, bool enable_forward )
+{
+	emit signalHistoryAvailabilityChanged( enable_backward, enable_forward );
+}
+
+
+#include "kchmviewwindow_khtmlpart.moc"
+
+#endif /* USE_KDE */
---
+++ src/kchmviewwindow_khtmlpart.h
@@ -0,0 +1,97 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Georgy Yunaev                                   *
+ *   tim@krasnogorsk.ru                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef KCHMVIEWWINDOW_KHTMLPART_H
+#define KCHMVIEWWINDOW_KHTMLPART_H
+
+#include "kde-qt.h"
+
+#include "kchmviewwindow.h"
+#include "kchmsourcefactory.h"
+
+/**
+@author Georgy Yunaev
+*/
+/*
+ * For Qt-only version, we cannot compile KCHMViewWindow_KHTMLPart. However,
+ * we cannot just exclude it, because it would not generate a MOC file for a KDE
+ * version. Therefore we declare it, and compile the .moc file, but do not provide
+ * the class methods.
+ */
+#if defined (USE_KDE)
+class KCHMViewWindow_KHTMLPart : public KHTMLPart, public KCHMViewWindow
+{
+Q_OBJECT
+public:
+	KCHMViewWindow_KHTMLPart( QWidget * parent );
+	~KCHMViewWindow_KHTMLPart();
+
+	//! Open a page from current chm archive
+	virtual bool	openPage (const QString& url);
+	
+	//! Invalidate current view, doing all the cleanups etc.
+	virtual void	invalidate();
+
+	//! Return current ZoomFactor.
+	virtual int		getZoomFactor() const { return m_zoomfactor; }
+	
+	//! Sets ZoomFactor. The value returned by getZoomFactor(), given to this function, should give the same result.
+	virtual void	setZoomFactor (int zoom);
+	
+	//! Relatively changes ZoomFactor. Most common values are -1 and 1.
+	virtual void	addZoomFactor (int value);
+
+	/*!
+	 * Return current scrollbar position in view window. Saved on program exit. 
+	 * There is no restriction on returned value, except that giving this value to 
+	 * setScrollbarPosition() should move the scrollbar in the same position.
+	 */
+	virtual int		getScrollbarPosition();
+	
+	//! Sets the scrollbar position.
+	virtual void	setScrollbarPosition(int pos);
+
+	virtual QObject *	getQObject() { return this; }
+
+	virtual QTextBrowser *	getQTextBrowser() { return 0; }
+	
+	virtual void	emitSignalHistoryAvailabilityChanged (bool enable_backward, bool enable_forward);
+
+signals:
+	/*!
+	 * Emitted when the user clicked on the link, before the page changed.
+ 	 * If signalLinkClicked() sets follow_link to false, the current page should NOT change.
+	 * Otherwise it should be changed to the new link value.
+	 */
+	void	signalLinkClicked ( const QString & newlink, bool& follow_link );
+	void	signalHistoryAvailabilityChanged (bool enable_backward, bool enable_forward);
+
+private slots:
+	virtual void	slotLinkClicked (const QString & newlink);
+	
+private:
+	void setSource ( const QString & name );
+			
+	int						m_zoomfactor;
+};
+
+#endif /* USE_KDE */
+
+#endif /* KCHMVIEWWINDOW_KHTMLPART_H */

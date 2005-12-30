db91646100db3e2f7e60119ded2c7e3103e4b30f
gyunaev <gyunaev@5804de7a-5b10-0410-9edc-facfe1f66358>
2005-12-30 09:40:58 +0000

    - Version changed to 1.4 - Fixed unknown symbols in kqrunprocess.h

--- trunk/kchmviewer/src/kqrunprocess.h
+++ trunk/kchmviewer/src/kqrunprocess.h
@@ -19,6 +19,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <stdlib.h>
+#include <unistd.h>
 
 static bool run_process( const QString& command, const QString& filename )
 {

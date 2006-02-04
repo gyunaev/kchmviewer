eb3ccb57c9d0f61815473fd6185254ad36235ed8
gyunaev <gyunaev@users.noreply.github.com>
2006-02-04 00:53:09 +0000

    - change autobuild test

--- tests/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf qt kde kde-int qt-int
-
-(mkdir kde && cd kde && ../../configure --with-kde && make) || exit 1;
-(mkdir qt && cd qt && ../../configure && make) || exit 1;
-
-(mkdir kde-int && cd kde-int && ../../configure --with-kde --with-builtin-chmlib && make) || exit 1;
-(mkdir qt-int && cd qt-int && ../../configure  --with-builtin-chmlib && make) || exit 1;
-

a7c27fc9ae171fdd1a0dfae6848306bb78fcf4c0
gyunaev <gyunaev@5804de7a-5b10-0410-9edc-facfe1f66358>
2009-10-24 22:22:11 +0000

    Version changed to 4.2

--- trunk/kchmviewer/src/version.h
+++ trunk/kchmviewer/src/version.h
@@ -5,7 +5,7 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
@@ -23,6 +23,6 @@
 #define INCLUDE_VERSION_H
 
 #define APP_NAME		"kchmviewer"
-#define APP_VERSION		"4.1"
+#define APP_VERSION		"4.2"
 
 #endif /* INCLUDE_VERSION_H */

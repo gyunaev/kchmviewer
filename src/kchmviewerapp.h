9afa3b69f603549f3846668cb1b06d1d460e5e0b
gyunaev <gyunaev@users.noreply.github.com>
2011-12-19 08:07:51 +0000

    Updated copyrights

--- src/kchmviewerapp.h
+++ src/kchmviewerapp.h
@@ -1,3 +1,27 @@
+/**************************************************************************
+ *  Kchmviewer - a portable CHM file viewer with the best support for     *
+ *  the international languages                                           *
+ *                                                                        *
+ *  Copyright (C) 2004-2012 George Yunaev, kchmviewer@ulduzsoft.com       *
+ *                                                                        *
+ *  Please read http://www.kchmviewer.net/reportbugs.html if you want     *
+ *  to report a bug. It lists things I need to fix it!                    *
+ *                                                                        *
+ *  This program is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *																	      *
+ *  This program is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ **************************************************************************/
+
+
 #ifndef KCHMVIEWERAPP_H
 #define KCHMVIEWERAPP_H
 
@@ -7,17 +31,17 @@
 class KchmviewerApp : public QApplication
 {
     Q_OBJECT
-public:
-    KchmviewerApp(int &argc, char **argv, int version= QT_VERSION);
-    virtual ~KchmviewerApp();
-    bool event(QEvent*);
+	public:
+		KchmviewerApp(int &argc, char **argv, int version= QT_VERSION);
+		virtual ~KchmviewerApp();
+		bool event(QEvent*);
 
-private slots:
-    void onTimer();
+	private slots:
+		void onTimer();
 
-private:
-    QString m_filePath;
-    int m_nResend;
+	private:
+		QString m_filePath;
+		int m_nResend;
 };
 
 #endif // KCHMVIEWERAPP_H

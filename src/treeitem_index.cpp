ac3c75b9c992a16bb864b6e861fa53f444a24df8
gyunaev <gyunaev@users.noreply.github.com>
2014-04-08 23:01:49 +0000

    Updated version and copyright

--- src/treeitem_index.cpp
+++ src/treeitem_index.cpp
@@ -1,3 +1,21 @@
+/*
+ *  Kchmviewer - a CHM and EPUB file viewer with broad language support
+ *  Copyright (C) 2004-2014 George Yunaev, gyunaev@ulduzsoft.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ebook.h"
 #include "mainwindow.h"
 #include "dialog_chooseurlfromlist.h"
@@ -54,6 +72,11 @@ bool TreeItem_Index::containstUrl(const QUrl &url) const
 	}
 
 	return false;
+}
+
+bool TreeItem_Index::isSeeAlso() const
+{
+	return m_seealso;
 }
 
 int TreeItem_Index::columnCount() const
